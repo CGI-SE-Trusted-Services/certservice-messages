3985ef036860b22647573ab51ef2c1bcbc9061ba
Philip Vendil <p.vendil@cgi.com>
2012-12-28 07:22:57 +0000

    Refactored project from certservice-pkimessages to a more generic certservice-messages in order to have more messages types in this project in the future.

---
+++ src/main/java/org/certificateservices/messages/DummyMessageSecurityProvider.java
@@ -0,0 +1,110 @@
+/************************************************************************
+*                                                                       *
+*  Certificate Service - PKI Messages                                   *
+*                                                                       *
+*  This software is free software; you can redistribute it and/or       *
+*  modify it under the terms of the GNU Affero General Public License   *
+*  License as published by the Free Software Foundation; either         *
+*  version 3   of the License, or any later version.                    *
+*                                                                       *
+*  See terms of license at gnu.org.                                     *
+*                                                                       *
+*************************************************************************/
+package org.certificateservices.messages;
+
+import java.security.KeyStore;
+import java.security.PrivateKey;
+import java.security.cert.X509Certificate;
+
+
+/**
+ * Dummy PKI Message Security Provider returning a self-signed certificate used
+ * for testing.
+ * 
+ * @author Philip Vendil
+ *
+ */
+public class DummyMessageSecurityProvider implements
+		MessageSecurityProvider {
+
+	private KeyStore dummyKS = null;
+	
+	private boolean validCallDone = false;
+	private String organisationCalled = null;
+	
+	private KeyStore getDummyKeystore() throws MessageException{
+		if(dummyKS == null){
+			try {
+				dummyKS = KeyStore.getInstance("JKS");
+				dummyKS.load(this.getClass().getResourceAsStream("/dummykeystore.jks"), "tGidBq0Eep".toCharArray());
+			} catch (Exception e) {
+				throw new MessageException("Error loading dummy key store: " + e.getMessage(),e);
+			}
+			
+		}
+		return dummyKS;
+	}
+	
+	/**
+	 * Method fetching the signing key from the dummy keystore.
+	 * 
+	 * @see org.certificateservices.messages.MessageSecurityProvider#getSigningKey()
+	 */
+	public PrivateKey getSigningKey() throws MessageException {
+	
+		try {
+			return (PrivateKey) getDummyKeystore().getKey("test", "tGidBq0Eep".toCharArray());
+		} catch (Exception e) {
+			throw new MessageException("Error fetching dummy signing key: " + e.getMessage(),e);
+		}
+	}
+
+	/**
+	 * 
+	 * @see org.certificateservices.messages.MessageSecurityProvider#getSigningCertificate()
+	 */
+	public X509Certificate getSigningCertificate()
+			throws IllegalArgumentException, MessageException {
+		try {
+			return (X509Certificate) getDummyKeystore().getCertificate("test");
+		} catch (Exception e) {
+			throw new MessageException("Error fetching dummy signing certificate: " + e.getMessage(),e);
+		}
+	}
+
+	/**
+	 * 
+	 * @see org.certificateservices.messages.MessageSecurityProvider#isValidAndAuthorized(X509Certificate)
+	 */
+	public boolean isValidAndAuthorized(X509Certificate signCertificate, String organisation)
+			throws IllegalArgumentException, MessageException {
+
+		if(signCertificate == null){
+			throw new IllegalArgumentException("Error sign certificate cannot be null when validating.");
+		}
+		
+		boolean[] keyUsage = signCertificate.getKeyUsage();
+		if (keyUsage[0] == false) {
+			return false;
+		}
+		
+		validCallDone = true;
+		organisationCalled = organisation;
+		
+		return true;
+	}
+	
+	public void resetCounters(){
+		validCallDone = false;
+		organisationCalled = null;
+	}
+	
+	public boolean getValidCallDone(){
+		return validCallDone;
+	}
+	
+	public String getOrganisationCalled(){
+		return organisationCalled;
+	}
+
+}
---
+++ src/main/java/org/certificateservices/messages/MessageSecurityProvider.java
@@ -0,0 +1,54 @@
+/************************************************************************
+*                                                                       *
+*  Certificate Service - PKI Messages                                   *
+*                                                                       *
+*  This software is free software; you can redistribute it and/or       *
+*  modify it under the terms of the GNU Affero General Public License   *
+*  License as published by the Free Software Foundation; either         *
+*  version 3   of the License, or any later version.                    *
+*                                                                       *
+*  See terms of license at gnu.org.                                     *
+*                                                                       *
+*************************************************************************/
+package org.certificateservices.messages;
+
+import java.security.PrivateKey;
+import java.security.cert.X509Certificate;
+
+
+/**
+ * PKI Message Security Provider used by the generator of messages to sign the PKI messages before 
+ * they are sent.
+ * 
+ * @author Philip Vendil
+ *
+ */
+public interface MessageSecurityProvider {
+	
+
+	/**
+	 * Fetches the signing key used to create the digital signatures of the XML file.
+	 * @return the signing key used.
+	 * @throws MessageException if key isn't accessible or activated.
+	 */
+	PrivateKey getSigningKey() throws MessageException;
+	
+	/**
+	 * Fetches the signing certificate used to create the digital signatures of the XML file.
+	 * @return the signing certificate used.
+	 * @throws MessageException if certificate isn't accessible.
+	 */
+	X509Certificate getSigningCertificate()  throws MessageException;
+	
+
+	/**
+	 * Method in charge of validating a certificate used to sign a PKI message
+	 * and also check if the certificate is authorized to generate messages.
+	 * @param signCertificate the certificate used to sign the message.
+	 * @param organisation the related organisation to the message
+	 * @return true if the 
+	 * @throws IllegalArgumentException if arguments were invalid.
+	 * @throws MessageException if internal error occurred validating the certificate.
+	 */
+	boolean isValidAndAuthorized(X509Certificate signCertificate, String organisation) throws IllegalArgumentException, MessageException;
+}
