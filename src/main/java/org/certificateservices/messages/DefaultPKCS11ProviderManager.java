8e4c29d68ff96982161da7460b229bbf47526ce2
Tobias Agerberg <tobias.agerberg@cgi.com>
2018-11-16 13:11:25 +0100

    Improved comments

--- src/main/java/org/certificateservices/messages/DefaultPKCS11ProviderManager.java
+++ src/main/java/org/certificateservices/messages/DefaultPKCS11ProviderManager.java
@@ -19,6 +19,10 @@ import java.io.InputStream;
 import java.security.*;
 import java.security.cert.CertificateException;
 
+/**
+ * PKCS#11 provider manager in charge of creating, adding and loading
+ * a java PKCS#11 keystore, using Sun PKCS#11.
+ */
 public class DefaultPKCS11ProviderManager implements PKCS11ProviderManager {
     /**
      * Create and add a PKCS#11 provider to the system
