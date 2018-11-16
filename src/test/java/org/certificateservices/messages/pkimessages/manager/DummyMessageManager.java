9dd1d2c425de7b4269eb7c4651c70829b58bde6b
Tobias Agerberg <tobias.agerberg@cgi.com>
2018-11-16 13:26:55 +0100

    Added license

--- src/test/java/org/certificateservices/messages/pkimessages/manager/DummyMessageManager.java
+++ src/test/java/org/certificateservices/messages/pkimessages/manager/DummyMessageManager.java
@@ -1,6 +1,15 @@
-/**
- * 
- */
+/************************************************************************
+ *                                                                       *
+ *  Certificate Service - Messages                                       *
+ *                                                                       *
+ *  This software is free software; you can redistribute it and/or       *
+ *  modify it under the terms of the GNU Affero General Public License   *
+ *  License as published by the Free Software Foundation; either         *
+ *  version 3   of the License, or any later version.                    *
+ *                                                                       *
+ *  See terms of license at gnu.org.                                     *
+ *                                                                       *
+ *************************************************************************/
 package org.certificateservices.messages.pkimessages.manager;
 
 import java.io.IOException;
