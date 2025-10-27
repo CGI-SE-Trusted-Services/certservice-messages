bfbb046ff3b67b2eb584f3f45513bf630a32d9e6
filipwessman <filip.wessman@cgi.com>
2025-10-27 12:54:56 +0100

    Made msgSender components overall more stable.

--- src/main/java/org/certificateservices/messages/utils/DefaultHTTPMsgSender.java
+++ src/main/java/org/certificateservices/messages/utils/DefaultHTTPMsgSender.java
@@ -10,11 +10,12 @@ import java.net.MalformedURLException;
 
 /**
  * DefaultHTTPMsgSender sending a byte[] array to a given URL.
- *
+ * <p>
  * Created by Philip Vendil on 16/06/16.
  */
 public class DefaultHTTPMsgSender extends BaseHTTPSender implements MsgSender {
 
+    static final String DEFAULT_CONTENT_TYPE = "text/xml; charset=utf-8";
 
     /**
      * Main constructor for POST requests
@@ -23,30 +24,27 @@ public class DefaultHTTPMsgSender extends BaseHTTPSender implements MsgSender {
      * @throws MalformedURLException if URL was malformed.
      */
     public DefaultHTTPMsgSender(String url) throws MalformedURLException {
-        super(url, "POST", "text/xml; charset=utf-8");
+        super(url, "POST", DEFAULT_CONTENT_TYPE);
     }
 
     /**
      * Main constructor where it is possible to specify request type.
      *
-     * @param url the URL to connect to.
+     * @param url         the URL to connect to.
      * @param requestType the HTTP request type in upper case (For example POST, GET)
      * @throws MalformedURLException if URL was malformed.
      */
     public DefaultHTTPMsgSender(String url, String requestType) throws MalformedURLException {
-        super(url, requestType, "text/xml; charset=utf-8");
-
+        super(url, requestType, DEFAULT_CONTENT_TYPE);
     }
 
     @Override
-    public byte[] sendMsg(byte[] request) throws MessageContentException, MessageProcessingException, IOException,
-            SpamProtectionException, TimeoutException {
+    public byte[] sendMsg(byte[] request) throws MessageContentException, MessageProcessingException, IOException, SpamProtectionException {
         return super.sendMsg(request);
     }
 
     @Override
     public void sendMsg(byte[] request, MsgCallback callback) {
-        super.sendMsg(request,callback);
+        super.sendMsg(request, callback);
     }
-
 }
