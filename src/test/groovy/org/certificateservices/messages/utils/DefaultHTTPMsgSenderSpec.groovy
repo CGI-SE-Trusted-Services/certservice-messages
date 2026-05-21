ce47d63f5834727aeea1e4e00325ce1b0a033b5e
fredrik.wickstrom <fredrik.wickstrom@cgi.com>
2026-05-21 14:53:52 +0200

    DEV-3107 dependency upgrades and clean up

--- src/test/groovy/org/certificateservices/messages/utils/DefaultHTTPMsgSenderSpec.groovy
+++ src/test/groovy/org/certificateservices/messages/utils/DefaultHTTPMsgSenderSpec.groovy
@@ -17,10 +17,10 @@ import org.certificateservices.messages.MessageProcessingException
 import org.certificateservices.messages.SpamProtectionException
 import org.certificateservices.messages.TimeoutException
 
-import javax.servlet.ServletException
-import javax.servlet.http.HttpServlet
-import javax.servlet.http.HttpServletRequest
-import javax.servlet.http.HttpServletResponse
+import jakarta.servlet.ServletException
+import jakarta.servlet.http.HttpServlet
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
 
 import org.certificateservices.messages.MessageContentException
 
